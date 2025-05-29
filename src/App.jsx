b5370ea962e300c47dc678bbde27ad637df02433
sandeepmanila <sandeep@techxplorers.in>
2025-05-29 16:12:19 +0530

    About & Contact Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import LandingPage from './components/LandingPage.jsx'; // Make sure the path is correct
+import AboutUs from './components/AboutUs.jsx';
+import ContactPage from './components/Contact.jsx';
 
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <ContactPage />
     </div>
   );
 }
