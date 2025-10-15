6f6fa008f5f741ca96833f84e80a6580d3399ca9
sandeepmanila <sandeep@techxplorers.in>
2025-10-15 12:24:43 +0530

    Version 1 TechXplorers Landing Page World Service Section 2

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -12,6 +12,19 @@ import { database } from '../firebase';
 import { ref, onValue } from "firebase/database";
 import { useInView } from 'react-intersection-observer'; // Ensure this is imported
 
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
+import shadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: iconRetina,
+    iconUrl: icon,
+    shadowUrl: shadow,
+});
+
 // --- Icon Components (omitted for brevity) ---
 // ... (Icon Components remain the same) ...
 const StarIcon = () => (
